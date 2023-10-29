479d37fad42a6148f8a209a70c0bbb566a2755fe
ZISHAN RAYEEN <136910246+zishanrayeen@users.noreply.github.com>
2023-10-29 21:36:49 +0530

    ALL FILES ADDED SUCESSFULLY

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function frist(){
+     
+    window.alert("✅ CLICK ON OK BUTTON 😄");
+
+}
+
+// developer console page
+console.log("DEVELOPED BY: Zishan Rayeen");
+console.log("DESIGNED BY: Sumit Kumar");
+
+
+function second() {
+
+    window.alert("CLICK ON OK, FOR REDRIECT IN NEXT PAGE ✅");
+
+}
